e82f5cb4d1dc5f0a3a8058e2ed695042467cf817
soong-e <sksuwjd@naver.com>
2022-09-30 15:40:23 +0900

    free pen

--- philo_bonus/manage_resource.c
+++ philo_bonus/manage_resource.c
@@ -6,7 +6,7 @@
 /*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 13:54:41 by yujelee           #+#    #+#             */
-/*   Updated: 2022/09/24 17:12:38 by yujelee          ###   ########seoul.kr  */
+/*   Updated: 2022/09/30 15:39:25 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@
 
 void	free_all(t_info *info, t_philo *philos)
 {
+	int	i;
+
+	i = -1;
+	while (++i < info->num)
+		free(philos[i].pen_num);
 	free(philos);
 	free(info->philo_id);
 }
